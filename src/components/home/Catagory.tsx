da65beba6e9d00a72ab167076ec070845a923630
hemnsoftare <138198540+hemnsoftare@users.noreply.github.com>
2024-11-18 09:14:15 +0300

    update to responsive

--- src/components/home/Catagory.tsx
+++ src/components/home/Catagory.tsx
@@ -39,9 +39,9 @@ const Catagory = () => {
   return (
     <div className="flex xl:gap-6 items-center lg:gap-3 md:gap-2 flex-wrap justify-center">
       {catagory.map((item) => (
-        <Link
+        <div
           key={item.path}
-          href={item.path}
+         
           className="2xl:w-[284px] lg:w-[140px] md:w-[100px] hover:shadow-blue-200 hover:shadow-lg duration-200 transition-all hover:transform hover:scale-[1.08] flex flex-col rounded-lg shadow-md border gap-4 border-slate-100 items-center justify-between md:px-2 lg:px-6 py-4"
         >
           <Image
