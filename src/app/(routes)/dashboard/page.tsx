c5ee7683a3f849e834d6d18f7ba19c0955d3d4fc
hemnsoftare <138198540+hemnsoftare@users.noreply.github.com>
2024-12-11 09:17:54 +0300

    .

--- src/app/(routes)/dashboard/page.tsx
+++ src/app/(routes)/dashboard/page.tsx
@@ -2,7 +2,11 @@ import React from "react";
 
 const page = () => {
   console.log("dashbaord");
-  return <div className="bg-red-50k">hi</div>;
+  return (
+    <div className="bg-red-50k">
+      <h3>jksdfjiklsdf</h3>
+    </div>
+  );
 };
 
 export default page;
